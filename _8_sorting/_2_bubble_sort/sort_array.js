a5bd8551cf8ac95dfcc2b66287771e1f0c084cc7
Aalia98 <aaliaamin98@gmail.com>
2025-03-16 11:10:59 +0530

    solves bubble sort optimized approach prblems

--- _8_sorting/_2_bubble_sort/sort_array.js
+++ _8_sorting/_2_bubble_sort/sort_array.js
@@ -1,9 +1,11 @@
 // https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort
 
+// brute force
+{
 const arr = [4,8,2,1,9,3,7];
 const n = arr.length;
 
-for(let i = n-1; i >= 1; i--) {
+for(let i = n-1; i >= 0; i--) {
     for(let j = 0; j <= i-1; j++) {
         if(arr[j] > arr[j+1]) {
             let temp = arr[j];
@@ -13,3 +15,26 @@ for(let i = n-1; i >= 1; i--) {
     }
 }
 console.log(arr);  // output: [1,2,3,4,7,8,9]
+}
+
+// optimized
+{
+    const arr = [1,2,3,4,5,6];
+    const n = arr.length;
+    
+    for(let i = n-1; i >= 0; i--) {
+        let didSwap = 0
+        for(let j = 0; j <= i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                didSwap = 1;
+            }
+        }
+        if(didSwap === 0) {
+            break;
+        }
+    }
+    console.log(arr);   // output: [1,2,3,4,5,6]
+}
